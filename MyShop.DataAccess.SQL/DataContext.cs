a3fb966179eaa40a02f43635d9301e2edc93fafd
bmaclin58@gmail.com <bmaclin58@gmail.com>
2019-03-05 13:59:20 -0500

    SQLExpress and Entity Framework for SQL database

---
+++ MyShop.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccess.SQL
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+            //looks at the web.config and finds the defination for DefaultConnection
+            :base("DefaultConnection"){
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+    }
+}
+//https://stackoverflow.com/questions/11231934/create-database-permission-denied-in-database-master-ef-code-first
