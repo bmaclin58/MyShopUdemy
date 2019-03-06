5116dce86ab1d93f380487c4e1fff57610b83490
bmaclin58@gmail.com <bmaclin58@gmail.com>
2019-03-06 10:40:20 -0500

    Basket Model created.

--- MyShop.DataAccess.SQL/DataContext.cs
+++ MyShop.DataAccess.SQL/DataContext.cs
@@ -18,6 +18,8 @@ namespace MyShop.DataAccess.SQL
 
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<BasketItem> BasketItem { get; set; }
     }
 }
 //https://stackoverflow.com/questions/11231934/create-database-permission-denied-in-database-master-ef-code-first
